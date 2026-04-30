76f9dc1a28d55ec84bd22f94d948675657eccdda
aayushamrute20-rgb <aayushamrute20@gmail.com>
2026-04-30 19:24:27 +0530

    Remove duplicate code for age calculation

--- age.py
+++ age.py
@@ -1,10 +1,3 @@
-'''
-from datetime import datetime
-by = int(input("Enter the Birth year: "))
-cy = datetime.now().year
-age = cy-by
-print(f"You are {age} years old.")
-'''
 from datetime import datetime
 by = int(input("Enter the Birth year: "))
 cy = datetime.now().year
